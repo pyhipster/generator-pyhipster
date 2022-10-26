617ebc450fbe510fd742a7d2e713ac07b1d63cb8
Sudeep Hazra <sudeephazra@gmail.com>
2022-10-26 22:55:29 +0530

    Removing prompt for documentation and donation

--- generators/generator-base.js
+++ generators/generator-base.js
@@ -1991,20 +1991,20 @@ module.exports = class JHipsterBaseGenerator extends PrivateBase {
       this.log(chalk.red('This can cause problems, you should always create a new directory and run the jhipster command from here.'));
       this.log(chalk.white(`See the troubleshooting section at ${chalk.yellow('https://www.jhipster.tech/installation/')}`));
     }
-    this.log(
-      chalk.green(' _______________________________________________________________________________________________________________\n')
-    );
-    this.log(
-      chalk.white(`  Documentation for creating an application is at ${chalk.yellow('https://www.jhipster.tech/creating-an-app/')}`)
-    );
-    this.log(
-      chalk.white(
-        `  If you find JHipster useful, consider sponsoring the project at ${chalk.yellow('https://opencollective.com/generator-jhipster')}`
-      )
-    );
-    this.log(
-      chalk.green(' _______________________________________________________________________________________________________________\n')
-    );
+    // this.log(
+    //   chalk.green(' _______________________________________________________________________________________________________________\n')
+    // );
+    // this.log(
+    //   chalk.white(`  Documentation for creating an application is at ${chalk.yellow('https://www.jhipster.tech/creating-an-app/')}`)
+    // );
+    // this.log(
+    //   chalk.white(
+    //     `  If you find JHipster useful, consider sponsoring the project at ${chalk.yellow('https://opencollective.com/generator-jhipster')}`
+    //   )
+    // );
+    // this.log(
+    //   chalk.green(' _______________________________________________________________________________________________________________\n')
+    // );
   }
 
   /**
