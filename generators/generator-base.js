5018e72d0587b998eb7387ab81e35aa23f3cf5ed
Sudeep Hazra <sudeephazra@gmail.com>
2022-10-28 12:35:41 +0530

    Cosmetic change to differentiate console output

--- generators/generator-base.js
+++ generators/generator-base.js
@@ -1992,9 +1992,9 @@ module.exports = class JHipsterBaseGenerator extends PrivateBase {
       this.log(chalk.red('This can cause problems, you should always create a new directory and run the jhipster command from here.'));
       this.log(chalk.white(`See the troubleshooting section at ${chalk.yellow('https://www.jhipster.tech/installation/')}`));
     }
-    // this.log(
-    //   chalk.green(' _______________________________________________________________________________________________________________\n')
-    // );
+    this.log(
+      chalk.green(' _______________________________________________________________________________________________________________\n')
+    );
     // this.log(
     //   chalk.white(`  Documentation for creating an application is at ${chalk.yellow('https://www.jhipster.tech/creating-an-app/')}`)
     // );
